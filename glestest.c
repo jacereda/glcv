5429003016a18fa608ec8036a67135c81a787795
Jorge Acereda <jacereda@gmail.com>
2011-10-01 02:16:23 +0200

    Added examples

---
+++ glestest.c
@@ -0,0 +1,85 @@
+#include "gs.h"
+#include "gsgl.h"
+
+static int s_running = 1;
+static GLuint g_prg;
+static GLint g_time;
+
+static GLuint loadShader(GLenum type, const char * src) {
+	GLuint sh = glCreateShader(type);
+	glShaderSource(sh, 1, &src, (void*)0);
+	glCompileShader(sh);
+	return sh;
+}
+
+int init(int argc, char ** argv) {
+	const char vss[] =
+		"attribute vec4 pos;   \n"
+		"uniform float t;            \n"
+		"void main()                 \n"
+		"{                           \n"
+		"   gl_Position = pos; \n"
+		"   gl_Position.y += sin(t) / 2.0; \n"
+		"}                           \n";
+	const char fss[] =
+		"void main()                                \n"
+		"{                                          \n"
+		" gl_FragColor = vec4(1.0, 1.0, 0.0, 1.0);  \n"
+		"}                                          \n";
+	GLuint vs, fs, prg;
+	vs = loadShader(GL_VERTEX_SHADER, vss);
+	fs = loadShader(GL_FRAGMENT_SHADER, fss);
+	prg = glCreateProgram();
+	glAttachShader(prg, vs);
+	glAttachShader(prg, fs);
+	glLinkProgram(prg);
+	g_prg = prg;
+	glBindAttribLocation(g_prg, 0, "vPosition");
+	g_time = glGetUniformLocation(g_prg, "t");
+	return 1;
+}
+
+int term() {
+	return 1;
+}
+
+int tick() {
+	return s_running;
+}
+
+void draw(void) {
+	GLfloat ver[] = {0.0f, 0.5f, 0.0f,
+			 -0.5f, -0.5f, 0.0f,
+			 0.5f, -0.5f, 0.0f};
+	static float t = 0;
+        static int i = 0;
+	glViewport(0, 0, gsWidth(), gsHeight());
+        glClearColor((i++ & 0xff) / 255.0f, 0.f, 1.f, 1.f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	t += 1/60.0;
+//	glClearColor(0.7, 0.7, 0.7, 1.0);
+//	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glUseProgram(g_prg);
+	glUniform1f(g_time, t);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, ver);
+	glEnableVertexAttribArray(0);
+	glDrawArrays(GL_TRIANGLES, 0, 3);
+	glFlush();
+}
+
+void event(const ev * e) {
+	switch (evType(e)) {
+	case GS_EVENT_CLOSE:
+		s_running = 0;
+		break;
+	default: break;
+	}
+}
+
+/* 
+   Local variables: **
+   c-file-style: "bsd" **
+   c-basic-offset: 8 **
+   End: **
+*/
---
+++ test.c
@@ -0,0 +1,31 @@
+#include "gs.h"
+#include "gsgl.h"
+
+static int s_running = 1;
+int init(int argc, char ** argv) {
+	return 1;
+}
+
+int term() {
+	return 1;
+}
+
+int tick() {
+	return s_running;
+}
+
+void draw() {
+        static int i = 0;
+        glClearColor((i++ & 0xff) / 255.0f, 0.f, 1.f, 0.f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glFlush();
+}
+
+void event(const ev * e) {
+	switch (evType(e)) {
+	case GS_EVENT_CLOSE:
+		s_running = 0;
+		break;
+	default: break;
+	}
+}
