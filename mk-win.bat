abd57a73864b89f52f46c4570089dbc6a4adef98
Jorge Acereda <jacereda@gmail.com>
2012-11-25 17:43:16 +0100

    Don't require bash for mk-win.bat

--- mk-win.bat
+++ mk-win.bat
@@ -1,7 +1,5 @@
-mk() {
-     gcc $* -I src src/win.c -lgdi32 -lopengl32
-}
-install -d b
-mk test/simple.c -o b/simple.exe
-mk test/test.c -o b/test.exe
-
+set MK=gcc $* -I src src/win.c 
+set LIBS=-lgdi32 -lopengl32
+mkdir b
+%MK% test/simple.c -o b/simple.exe %LIBS
+%MK% test/test.c -o b/test.exe %LIBS
