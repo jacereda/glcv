e62b5b890415fdff7db169b20bad3ffa6308b067
Jorge Acereda <jacereda@gmail.com>
2020-08-31 20:18:53 +0200

    Add missing lib

--- mk-win.bat
+++ mk-win.bat
@@ -1,5 +1,5 @@
 set MK=clang -O2 -Wall -I src -D_CRT_SECURE_NO_WARNINGS src/win.c
-set LIBS=-lgdi32 -lopengl32 -luser32
+set LIBS=-lgdi32 -lopengl32 -luser32 -ldwmapi
 mkdir b
 %MK% test/simple.c -o b/simple.exe %LIBS%
 %MK% test/test.c -o b/test.exe %LIBS%
