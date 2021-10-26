8d8b00e991dcd2977e94f1d4068cbfe09b38ec5f
Jorge Acereda <jacereda@gmail.com>
2021-10-26 22:59:04 +0200

    Add dynamic symbol headers

---
+++ src/dyn.h
@@ -0,0 +1,3 @@
+D(ogl, HGLRC, wglCreateContextAttribsARB, (HDC dc, HGLRC sh, const int * attr))
+D(ogl, HGLRC, wglGetCurrentContext, ())
+D(ogl, int, wglSwapIntervalEXT, (int si))
---
+++ src/earlydyn.h
@@ -0,0 +1,4 @@
+D(ogl, void *, wglGetProcAddress, (const char * n))
+D(ogl, HGLRC, wglCreateContext, (HDC dc))
+D(ogl, BOOL, wglMakeCurrent, (HDC dc, HGLRC rc))
+// D(ogl, int, wglSwapIntervalEXT)
